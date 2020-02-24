a0680abf9eb6eeab67a77fce57b02a8b40859001
Andrii Iavorskii <aiavorskii@devengineering.com>
2020-02-24 19:59:04 +0200

    Adding action link to followers scrapper form

--- resources/views/form/form-counter.blade.php
+++ resources/views/form/form-counter.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div style="height: 100vh;" class="col-lg-12 d-flex align-items-center justify-content-center">
-                <form action="/">
+                <form action="{{ $action }}">
                     <div class="form-group">
                         <label for="web-link-field">Profile to parse</label>
                         <input type="text" class="form-control" name="profile" id="web-link-field" placeholder="www.example.com">
